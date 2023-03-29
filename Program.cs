e1e5c7fff8674a08f4bf7ee62ddfcb4d2ce63e50
sipra <sipramishra085@gmail.com>
2023-03-29 23:54:06 +0530

    Added program file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Day14DataStructureProblem;
+
+namespace Day14DataStuctureProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue program:");
+            Console.WriteLine("********");
+
+            StackAddLinkedList obj = new StackAddLinkedList();
+            obj.push(70);
+            obj.push(30);
+            obj.push(56);
+            obj.Display();
+
+        }
+    }
+}
